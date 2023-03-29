b8058f436af3fab4f075334babe898dfe3b9daad
Anudeep Nalla <121174814+anuddeeph1@users.noreply.github.com>
2023-03-30 01:12:44 +0530

    Update eksctl-updatecm-sa.sh

--- karpenter-pre-req/eksctl-updatecm-sa.sh
+++ karpenter-pre-req/eksctl-updatecm-sa.sh
@@ -3,16 +3,15 @@ CLUSTER_NAME=$1
 ACCOUNT_ID=$2
 
 update-awsauth-cm(){
-    echo "Updating awsauth configmap"  
+    echo "Updating awsauth configmap"
     eksctl create iamidentitymapping \
       --username system:node:{{EC2PrivateDNSName}} \
       --cluster  ${CLUSTER_NAME} \
       --arn "arn:aws:iam::${ACCOUNT_ID}:role/KarpenterNodeRole-${CLUSTER_NAME}" \
       --group system:bootstrappers \
       --group system:nodes
-}     
+}
 
-{
 KarpenterController-IAM-Role(){
     echo "Creating KarpenterController IAM Role for the service account"
     eksctl create iamserviceaccount \
@@ -21,11 +20,11 @@ KarpenterController-IAM-Role(){
       --attach-policy-arn "arn:aws:iam::${ACCOUNT_ID}:policy/KarpenterControllerPolicy-${CLUSTER_NAME}" \
       --role-only \
       --approve
-}    
+}
 
-if [ $# -lt 2  ]; then 
-  echo "Provide Cluster Name and Account ID as arguments"; 
-else 
+if [[ $# != 2 ]]; then
+  echo "Provide Cluster Name and Account ID as arguments";
+else
   update-awsauth-cm;
   KarpenterController-IAM-Role;
 fi
